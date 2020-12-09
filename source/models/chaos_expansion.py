867893c2d1a5777919e121a389ad6f95d31cf3f2
tommasorea <75129093+tommasorea@users.noreply.github.com>
2020-12-09 15:40:03 +0000

    comments, docstring and markdown
    docstrings for chaos_expansion.py, high_fidelity.py, and part of poisson_multifidelity.py, markdown on poisson_multifidelity.ipynb

--- source/models/chaos_expansion.py
+++ source/models/chaos_expansion.py
@@ -7,7 +7,34 @@ from .base_model import ForwardModel
 
 
 class PCESurrogate(ForwardModel):
+    #es. di parametri passati: data, log_err_dens, prior, log_prior, 2, 10
+    #da capire meglio cosa sono i multi_fidelity_q
+    #degree dovrebbe essere il grado dei polinomi phi_n che uso in polyn chaos exp
+
+    #mentre multi_fidelity_q dovrebbe essere il num di pti che estraggo quando faccio
+    #l'update del low-fidelity quando uso l'adaptive multi_fidelity
     def __init__(self, data, log_error_density, prior, log_prior, degree, multi_fidelity_q):
+        """
+        This class represents the low-fidelity model, approximating the forward
+        model through polynomial chaos expansion. It has methods to fit the
+        low-fidelity model, to evaluate it in a given parameter, to compute
+        the posterior (based on low-fidelity) of a parameter and a method to
+        update the model (called by the multi fidelity algorithm).
+        :param data: numpy.array, sample of observed values of the forward model
+                     in the spatial nodes
+        :param log_error_density: function, it computes the log density at the input
+                                  vector (used for computing the likelihood, passing
+                                  as input the difference between the data and the
+                                  vector of the evaluations of the surrogate model
+                                  in the corresponding spatial nodes for a fixed
+                                  parameter z)
+        :param prior: chaospy.Iid, object that represents the prior of the parameter
+        :param log_prior: function, it computes the log prior at the input vector
+                          (usually vector of parameters)
+        :param degree: int, it indicates the degree of the polynomial expansions
+        :param multi_fidelity_q: int, it indicates the number of drawn parameter
+                                 samples when I update the low fidelity model
+        """
         super().__init__()
         self.data = data
         self.log_error_density = log_error_density
@@ -21,21 +48,51 @@ class PCESurrogate(ForwardModel):
         self._fit = False
 
     @staticmethod
+    #fitted_poly è la somma dei polinomi phi_n
     def _get_coeffs(fitted_poly):
         return np.concatenate([np.array(p.coefficients).reshape(1, -1) for p in fitted_poly], axis=0)
 
+#da capire meglio cos'è quadrature_rule
+    #dovrebbe essere: ∫ phi_i(z)phi_j(z) f_Z(z) dz= 0 (ortogonalità), con f_Z= prior
+    #e con quadrature_rule='gaussian' cosa intendo?
+
     def fit(self, high_fidelity, quadrature_rule='gaussian'):
+        """
+        Method for fitting the low-fidelity model basing on polynomial chaos
+        expansion.
+        :param high_fidelity: HighFidelityModel, model that we want to approximate
+                              through the low-fidelity
+        :param quadrature_rule: str, rule used for the quadrature
+        :return: none
+        """
+        #come faccio a sapere il Q (numero di punti per la stima dei coeff) del paper, cioè
+        #la lunghezza di abscissae? lo fa da solo sulla base del grado? perché devo avere
+        #Q>numero di polinomi
+
         abscissae, weights = cpy.generate_quadrature(self.degree, self.prior, rule=quadrature_rule)
         self.expansion = generate_expansion(self.degree, self.prior, retall=False)
         evals = [high_fidelity.eval(z_) for z_ in abscissae.T]
+        #come entrano i weights nel problema di minimizzazione?
+        #cioè a questo punto ho z1,...,zQ; w1,...,wQ e G(z1),...., G(zQ)
+        #secondo me entrano nella funzione di minimizzazione, dando più peso
+        #agli errori commessi negli zj con peso maggiore
         self.proxy = cpy.fit_quadrature(self.expansion, abscissae, weights, evals)
         self._fit = True
 
     def eval(self, z):
+        """
+        Member function that returns the evaluations of the low-fidelity model for
+        the parameter passed as input, in the evaluation nodes
+        :param z: numpy.array, parameter for which I want to compute the values of
+                  the low-fidelity model in the evaluation nodes
+        :return: numpy.array, it contains the values of the low-fidelity model in
+                 the evaluation nodes
+        """
         assert self._fit
         if self.last_evalutations[0][0] is not None and np.all(
                 np.abs(z - self.last_evalutations[0][0]) < np.finfo(np.float32).eps):
             pass
+        # condition to avoid repeated evaluations for the same parameter
         elif self.last_evalutations[1][0] is not None and np.all(
                 np.abs(z - self.last_evalutations[1][0]) < np.finfo(np.float32).eps):
             self.last_evalutations.reverse()
@@ -45,15 +102,37 @@ class PCESurrogate(ForwardModel):
         return self.last_evalutations[0][1]
 
     def logposterior(self, z):
+        """
+        Member function that returns the value of the log posterior for the parameter
+        passed as input, where the likelihood is based on the low-fidelity model
+        :param z: numpy.array, parameter for which I want to compute the log of the
+                  posterior
+        :return: numpy.float, value of the log of the posterior
+        """
         predicted = self.eval(z)
         res = self.log_error_density(self.data - predicted) + self.log_prior(z)
         return res
 
     def multi_fidelity_update(self, y, radius, high_fidelity):
+        """
+        Member function that updates the low-fidelity, based on the polynomial chaos
+        expansion of the difference between the high and low fidelity models.
+        It uses a regression method on new points drawn from a uniform
+        distribution centered on y with radius passed as input
+        :param y: float, value of the center used for drawing the sample
+        :param radius: float, value of the radius used for drawing the sample
+        :param high_fidelity: HighFidelityModel, model that we want to approximate
+                              with the low-fidelity
+        :return: none
+        """
         if type(y) is np.ndarray:
             new_points = y.reshape(-1, 1) + np.random.uniform(-radius, radius, (y.shape[0], self.multi_fidelity_q))
         else:
             new_points = y + np.random.uniform(-radius, radius, self.multi_fidelity_q)
+       #questo è nel paper C(z_)_i per ogni nodo spaziale i per ogni z_ estratto dalla palla
+        #centrata in y
         new_evals = [high_fidelity.eval(z_) - self.eval(z_) for z_ in new_points.T]
+       #perché qui uso fit_regression anziché fit_quadrature per fare il pce
+       #per l'errore ?
         new_poly = cpy.fit_regression(self.expansion, new_points, new_evals)
         self.proxy = self.proxy + new_poly
--- source/models/high_fidelity.py
+++ source/models/high_fidelity.py
@@ -4,7 +4,30 @@ from .base_model import ForwardModel
 
 
 class HighFidelityModel(ForwardModel):
+    #esempi di parametri che passo sono: forward_model, data, x, log_err_dens, log_prior;
+    #perciò la fz(sol u della pde), dati (valori di u nei nodi), nodi spaziali, πe, e prior
     def __init__(self, core_function, data, evaluation_nodes, log_error_density, log_prior):
+        """
+        This class represents the high fidelity model that in our case coincides
+        with the true forward model. We build methods to evaluate the forward
+        model in a given parameter and the posterior of the parameter.
+        :param core_function: ForwardModel, is our true forward model
+        :param data: numpy.array, sample of observed values of the forward model
+                     in the spatial nodes
+        :param evaluation_nodes: numpy.array, matrix of the spatial nodes in
+                                 which I have the observed data. Every column is
+                                 a node
+        :param log_error_density: function, is a function that requires as input
+                                  a vector and returns the log density computed on
+                                  it (used for computing the likelihood, passing as
+                                  input the difference between the data and the
+                                  vector of the evaluations of the forward model in
+                                  the corresponding spatial nodes for a fixed
+                                  parameter z)
+        :param log_prior: function, is a function that requires in input a vector
+                          (usually vector of parameters) and returns the log density
+                          computed on it
+        """
         # make sure that the data nodes are a subset of the quadrature nodes
         super().__init__()
         self.core_function = core_function
@@ -12,22 +35,51 @@ class HighFidelityModel(ForwardModel):
         self.evaluation_nodes = evaluation_nodes
         self.log_error_density = log_error_density
         self.log_prior = log_prior
+        #perché una valutaione è una coppia? (None,None)
+        #forse perché nella coppia salvo come primo elemento il parametro z,
+        #e come secondo elemento u(z) per ciascun nodo
         self.last_evalutations = [(None, None), (None, None)]       # always keep the last two evaluations in memory
 
+#perché confronto lo z con gli ultimi due parametri?
+#quando questa cosa è ricorrente nell'algoritmo?
+    #il motivo credo sia che nel mh quando calcolo alpha al denominatore ho z_i-1
+    #e non voglio ricalcolarmelo
+#ed in ogni caso perché non solo con l'ultimo dei parametri ma con gli ultimi 2?
+    #forse è dovuto al fatto che se poi rigetto vuol dire che l'ultima valutaz è
+    #in z* mentre la penultima è in z_i-1 e in caso di rigetto sono interessato
+    #quindi alla penultima
     def eval(self, z):
+        """
+        Member function that returns the evaluations of the forward model for the
+        parameter passed as input, in the evaluation nodes
+        :param z: numpy.array, parameter for which I want to compute the values of
+                  the forward model in the evaluation nodes
+        :return: numpy.array, it contains the values of the forward model in the
+                 evaluation nodes
+        """
         if self.last_evalutations[0][0] is not None and np.all(
                 np.abs(z - self.last_evalutations[0][0]) < np.finfo(np.float32).eps):
             pass
+        # condition to avoid repeated evaluations for the same parameter
         elif self.last_evalutations[1][0] is not None and np.all(
                 np.abs(z - self.last_evalutations[1][0]) < np.finfo(np.float32).eps):
             self.last_evalutations.reverse()
         else:
             self.last_evalutations[1] = (z, self.core_function(z, self.evaluation_nodes))
             self.last_evalutations.reverse()
-        return self.last_evalutations[0][1]     # self.last_evalutations[0] is the most recent entry
+        return self.last_evalutations[0][1]
+        # self.last_evalutations[0] is the most recent entry
 
+#la posterior è prior(z)*πe(d-G(z)) ==> log(post) = log(prior) + log(πe)
     def logposterior(self, z):
-        predicted = self.eval(z)
+        """
+        Member function that returns the value of the log posterior for the parameter
+        passed as input
+        :param z: numpy.array, parameter for which I want to compute the log of the
+                  posterior
+        :return: numpy.float, value of the log of the posterior
+        """
+        predicted = self.eval(z) # qui ottengo G(x_i;z)
         res = self.log_error_density(self.data - predicted) + self.log_prior(z)       # implement it via mpi
         return res
 
